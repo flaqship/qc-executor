1493d0a1c06c57ca28384407e0dfc970fee02273
Dennis Kleinhans <dennis.kleinhans@ipa.fraunhofer.de>
2026-01-14 08:49:59 +0100

    add `QiskitObservable` wrapper

---
+++ src/executor/qiskit/qiskit_observable.py
@@ -0,0 +1,110 @@
+from collections import OrderedDict
+from typing import List
+import numpy as np
+
+
+class QiskitObservable:
+    """
+    Wrapper for Qiskit observables (SparsePauliOp) used by QiskitExecutor.
+    Handles parameter management for parametrized operators.
+    """
+
+    def __init__(self, observable):
+        """
+        Initialize QiskitObservable wrapper.
+
+        Args:
+            observable: QuantumOperator object (from executor.quantum_operator)
+        """
+        # Extract the internal qiskit operator
+        if hasattr(observable, "_qiskit_operator"):
+            self._qiskit_operator = observable._qiskit_operator
+        else:
+            self._qiskit_operator = observable
+
+        self._num_qubits = self._qiskit_operator.num_qubits
+
+        # Group parameters by ParameterVector name
+        self._parameter_dimensions = OrderedDict()
+        self._free_parameters = set()
+
+        for p in self._qiskit_operator.parameters:
+            self._free_parameters.add(p)
+            name = p.vector.name
+            self._parameter_dimensions[name] = self._parameter_dimensions.get(name, 0) + 1
+
+    @property
+    def num_qubits(self) -> int:
+        """Number of qubits the operator acts on."""
+        return self._num_qubits
+
+    @property
+    def hash(self) -> int:
+        """Hash of the operator for caching."""
+        return hash(str(self._qiskit_operator))
+
+    @property
+    def parameter_names(self) -> List[str]:
+        """List of parameter vector names."""
+        return list(self._parameter_dimensions.keys())
+
+    @property
+    def parameter_dimensions(self) -> dict:
+        """Dictionary mapping parameter names to their dimensions."""
+        return dict(self._parameter_dimensions)
+
+    @property
+    def qiskit_operator(self):
+        """Access to the underlying Qiskit SparsePauliOp."""
+        return self._qiskit_operator
+
+    @property
+    def free_parameters(self) -> set:
+        """Set of all free parameters in the operator."""
+        return self._free_parameters
+
+    def bind_parameters(self, parameter_values: dict):
+        """
+        Bind parameter values to the operator.
+
+        Args:
+            parameter_values: Dictionary mapping parameter names to values
+
+        Returns:
+            Bound Qiskit SparsePauliOp
+        """
+        # Convert parameter_values dict (with vector names) to Qiskit format
+        params_dict = {}
+
+        for param_name, values in parameter_values.items():
+            # Ensure values is a list
+            if not isinstance(values, (list, np.ndarray)):
+                values = [values]
+
+            # Match parameters from operator with provided values
+            matching_params = [p for p in self._free_parameters if p.vector.name == param_name]
+
+            # Sort by index to ensure correct ordering
+            matching_params = sorted(matching_params, key=lambda x: x.index)
+
+            for i, param in enumerate(matching_params):
+                if i < len(values):
+                    params_dict[param] = values[i]
+
+        # Bind parameters to operator
+        if params_dict:
+            return self._qiskit_operator.assign_parameters(params_dict)
+        else:
+            return self._qiskit_operator
+
+    def copy(self):
+        """Return a copy of the observable wrapper."""
+        return QiskitObservable(self._qiskit_operator.copy())
+
+    def __str__(self):
+        return str(self._qiskit_operator)
+
+    def __repr__(self):
+        return (
+            f"QiskitObservable({self.num_qubits} qubits, {len(self._free_parameters)} parameters)"
+        )

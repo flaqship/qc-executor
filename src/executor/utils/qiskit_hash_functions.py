b62a4a0be9b5e8467a26e386e436c4e6a62753d6
Dennis Kleinhans <dennis.kleinhans@ipa.fraunhofer.de>
2026-02-04 11:15:53 +0100

    update `qiskit_hash_functions`

--- src/executor/utils/qiskit_hash_functions.py
+++ src/executor/utils/qiskit_hash_functions.py
@@ -1,81 +1,86 @@
-from collections.abc import Iterable
-
 import numpy as np
+from collections.abc import Iterable
+from packaging import version
 
 from qiskit.circuit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
-from qiskit.circuit.bit import Bit
+from qiskit import __version__ as qiskit_version
 
-# TODO: copied from QISKIT adjust for differnet versions of qiskit!
+QISKIT_SMALLER_2_0 = version.parse(qiskit_version) < version.parse("2.0.0")
 
+if QISKIT_SMALLER_2_0:
+    # pylint: disable=ungrouped-imports
+    from qiskit.primitives.utils import _circuit_key, _observable_key
+else:
 
-def _bits_key(bits: tuple[Bit, ...], circuit: QuantumCircuit) -> tuple:
-    return tuple(
-        (
-            circuit.find_bit(bit).index,
-            tuple((reg[0].size, reg[0].name, reg[1]) for reg in circuit.find_bit(bit).registers),
-        )
-        for bit in bits
-    )
-
-
-def _format_params(param):
-    if isinstance(param, np.ndarray):
-        return param.data.tobytes()
-    elif isinstance(param, QuantumCircuit):
-        return _circuit_key(param)
-    elif isinstance(param, Iterable):
-        return tuple(param)
-    return param
-
-
-def _circuit_key(circuit: QuantumCircuit, include_name=False) -> tuple:
-    """Private key function for QuantumCircuit.
-
-    This is the workaround until :meth:`QuantumCircuit.__hash__` will be introduced.
-    If key collision is found, please add elements to avoid it.
-
-    Args:
-        circuit: Input quantum circuit.
-        functional: If True, the returned key only includes functional data (i.e. execution related).
-
-    Returns:
-        Composite key for circuit.
-    """
-    circuit_tuple = (
-        circuit.num_qubits,
-        circuit.num_clbits,
-        circuit.num_parameters,
-        tuple(  #
+    def _bits_key(bits: tuple, circuit: QuantumCircuit) -> tuple:
+        return tuple(
             (
-                _bits_key(data.qubits, circuit),  # qubits
-                _bits_key(data.clbits, circuit),  # clbits
-                data.operation.name,  # operation.name
+                circuit.find_bit(bit).index,
                 tuple(
-                    _format_params(param) for param in data.operation.params
-                ),  # operation.params
+                    (reg[0].size, reg[0].name, reg[1]) for reg in circuit.find_bit(bit).registers
+                ),
             )
-            for data in circuit.data
-        ),
-        None if circuit._op_start_times is None else tuple(circuit._op_start_times),
-    )
-    if not include_name:
-        return circuit_tuple
-    else:
-        return (circuit.name, *circuit_tuple)
+            for bit in bits
+        )
 
+    def _format_params(param):
+        if isinstance(param, np.ndarray):
+            return param.data.tobytes()
+        elif isinstance(param, QuantumCircuit):
+            return _circuit_key(param)
+        elif isinstance(param, Iterable):
+            return tuple(param)
+        return param
 
-def _observable_key(observable: SparsePauliOp) -> tuple:
-    """Private key function for SparsePauliOp.
-    Args:
-        observable: Input operator.
+    def _circuit_key(circuit: QuantumCircuit, functional: bool = True) -> tuple:
+        """Private key function for QuantumCircuit.
 
-    Returns:
-        Key for observables.
-    """
-    return (
-        observable.paulis.z.tobytes(),
-        observable.paulis.x.tobytes(),
-        observable.paulis.phase.tobytes(),
-        observable.coeffs.tobytes(),
-    )
+        This is the workaround until :meth:`QuantumCircuit.__hash__` will be introduced.
+        If key collision is found, please add elements to avoid it.
+
+        Args:
+            circuit: Input quantum circuit.
+            functional: If True, the returned key only includes functional data (i.e. execution related).
+
+        Returns:
+            Composite key for circuit.
+        """
+        functional_key: tuple = (
+            circuit.num_qubits,
+            circuit.num_clbits,
+            circuit.num_parameters,
+            tuple(  # circuit.data
+                (
+                    _bits_key(data.qubits, circuit),  # qubits
+                    _bits_key(data.clbits, circuit),  # clbits
+                    data.operation.name,  # operation.name
+                    tuple(
+                        _format_params(param) for param in data.operation.params
+                    ),  # operation.params
+                )
+                for data in circuit.data
+            ),
+            None if circuit._op_start_times is None else tuple(circuit._op_start_times),
+        )
+        if functional:
+            return functional_key
+        return (
+            circuit.name,
+            *functional_key,
+        )
+
+    def _observable_key(observable: SparsePauliOp) -> tuple:
+        """Private key function for SparsePauliOp.
+        Args:
+            observable: Input operator.
+
+        Returns:
+            Key for observables.
+        """
+        return (
+            observable.paulis.z.tobytes(),
+            observable.paulis.x.tobytes(),
+            observable.paulis.phase.tobytes(),
+            observable.coeffs.tobytes(),
+        )

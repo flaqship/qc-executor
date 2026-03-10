a32f995e9047af828368d6602f086ff4bcd6975c
Dennis Kleinhans <dennis.kleinhans@ipa.fraunhofer.de>
2026-03-10 11:46:23 +0100

    refactor: update type hinting for qiskit-ibm-runtime version flags

--- src/executor/utils/qiskit_compat.py
+++ src/executor/utils/qiskit_compat.py
@@ -8,6 +8,7 @@ blocks and version-gated imports.
 Supported Qiskit versions: >= 1.0  (including 2.x).
 """
 
+from typing import Union
 from packaging import version
 from qiskit import __version__ as qiskit_version
 from qiskit.circuit import ParameterExpression
@@ -22,9 +23,9 @@ QISKIT_SMALLER_2_0 = version.parse(qiskit_version) < version.parse("2.0.0")
 # The flags are set to ``None`` when the package is not available and
 # should be treated as "feature not available".
 QISKIT_RUNTIME_AVAILABLE: bool = False
-QISKIT_RUNTIME_SMALLER_0_21: bool | None = None
-QISKIT_RUNTIME_SMALLER_0_23: bool | None = None
-QISKIT_RUNTIME_SMALLER_0_28: bool | None = None
+QISKIT_RUNTIME_SMALLER_0_21: Union[bool, None] = None
+QISKIT_RUNTIME_SMALLER_0_23: Union[bool, None] = None
+QISKIT_RUNTIME_SMALLER_0_28: Union[bool, None] = None
 
 try:
     from qiskit_ibm_runtime import __version__ as _ibm_runtime_version  # type: ignore[import-untyped]

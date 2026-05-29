ce67a184f3050717835ee07bb60deedd81af75ed
ProfessorNova <114916947+ProfessorNova@users.noreply.github.com>
2026-05-29 15:18:53 +0200

    lint src\executor\qiskit\qiskit_operator.py

--- src/executor/qiskit/qiskit_operator.py
+++ src/executor/qiskit/qiskit_operator.py
@@ -1,3 +1,5 @@
+"""Qiskit operator wrapper for use with QiskitExecutor."""
+
 from __future__ import annotations
 
 from collections import OrderedDict
@@ -102,8 +104,7 @@ class QiskitOperator:
         # Bind parameters to operator
         if params_dict:
             return self._qiskit_operator.assign_parameters(params_dict)
-        else:
-            return self._qiskit_operator
+        return self._qiskit_operator
 
     def copy(self):
         """Return a copy of the operator wrapper."""

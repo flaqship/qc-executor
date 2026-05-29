0e01de2f2e8a016ade225dd8f3beb31ca8893cd6
ProfessorNova <114916947+ProfessorNova@users.noreply.github.com>
2026-05-29 15:36:58 +0200

    Solve R0801: Similar lines in 2 files

--- src/executor/pennylane/pennylane_circuit.py
+++ src/executor/pennylane/pennylane_circuit.py
@@ -5,94 +5,16 @@ from __future__ import annotations
 from typing import Any, Callable, Optional, cast
 
 import pennylane as qml
-import pennylane.numpy as pnp
 from qiskit import transpile
 from qiskit.circuit import Clbit, ParameterExpression
 from qiskit.circuit import QuantumCircuit as QiskitQuantumCircuit
 from sympy import lambdify
-from sympy.printing.numpy import NumPyPrinter as _NumPyPrinter
 
 from ..quantum_circuit import QuantumCircuit
 from ..utils.decompose_to_std import decompose_to_std
 from ..utils.qiskit_compat import _param_is_constant, _param_to_float, _param_to_sympy
+from ._sympy_interface import _get_sympy_interface
 from .pennylane_gates import pennylane_target, qiskit_pennylane_gate_dict
-
-
-def _get_sympy_interface():
-    """
-    Returns the sympy interface that is used in the parameter conversion.
-
-    Necessary for the correct conversion of sympy expressions in Qiskit to
-    python functions in PennyLane.
-
-    Returns:
-        Tuple of sympy printer and sympy modules
-    """
-    # SymPy printer for pennylane numpy implementation has to be set manually,
-    # otherwise math functions are used in lambdify instead of pennylane.numpy functions
-    user_functions = {}
-    printer = _NumPyPrinter(
-        {
-            "fully_qualified_modules": False,
-            "inline": True,
-            "allow_unknown_functions": True,
-            "user_functions": user_functions,
-        }
-    )
-    # Use Pennylane numpy for sympy lambdify
-    modules = pnp
-
-    # The functions down below can be used to switch between different gradient engines
-    # as tensorflow, jax and torch. However, this is not supported and implemented yet.
-
-    #     # SymPy printer for pennylane numpy implementation has to be set manually,
-    #     # otherwise math functions are used in lambdify instead of pennylane.numpy functions
-    #     from sympy.printing.tensorflow import TensorflowPrinter as Printer  # type: ignore
-
-    #     user_functions = {}
-    #     printer = Printer(
-    #         {
-    #             "fully_qualified_modules": False,
-    #             "inline": True,
-    #             "allow_unknown_functions": True,
-    #             "user_functions": user_functions,
-    #         }
-    #     )  #
-    #     modules = tf
-
-    # elif self._gradient_engine == "jax":
-    #     from sympy.printing.numpy import JaxPrinter as Printer  # type: ignore
-
-    #     user_functions = {}
-    #     printer = Printer(
-    #         {
-    #             "fully_qualified_modules": False,
-    #             "inline": True,
-    #             "allow_unknown_functions": True,
-    #             "user_functions": user_functions,
-    #         }
-    #     )  #
-    #     modules = jnp
-    # elif self._gradient_engine == "torch" or self._gradient_engine == "pytorch":
-    #     from sympy.printing.pycode import PythonCodePrinter as Printer  # type: ignore
-
-    #     user_functions = {}
-    #     printer = Printer(
-    #         {
-    #             "fully_qualified_modules": False,
-    #             "inline": True,
-    #             "allow_unknown_functions": True,
-    #             "user_functions": user_functions,
-    #         }
-    #     )  #
-    #     modules = torch
-
-    # else:
-    #     # tbd for jax and tensorflow
-    #     printer = None
-    #     modules = None
-
-    return printer, modules
 
 
 class PennyLaneCircuit:
--- src/executor/pennylane/pennylane_operator.py
+++ src/executor/pennylane/pennylane_operator.py
@@ -11,90 +11,13 @@ from pennylane import pauli
 from qiskit.circuit import ParameterExpression
 from qiskit.quantum_info import SparsePauliOp
 from sympy import lambdify
-from sympy.printing.numpy import NumPyPrinter as _NumPyPrinter
 
 from ..base import QuantumOperatorBase
 from ..utils.qiskit_compat import _param_is_constant, _param_to_sympy
+from ._sympy_interface import _get_sympy_interface
 
 if TYPE_CHECKING:
     from ..quantum_operator import QuantumOperator
-
-
-def _get_sympy_interface():
-    """
-    Returns the sympy interface that is used in the parameter conversion.
-
-    Necessary for the correct conversion of sympy expressions in Qiskit to
-    python functions in PennyLane.
-
-    Returns:
-        Tuple of sympy printer and sympy modules
-    """
-    # SymPy printer for pennylane numpy implementation has to be set manually,
-    # otherwise math functions are used in lambdify instead of pennylane.numpy functions
-    user_functions = {}
-    printer = _NumPyPrinter(
-        {
-            "fully_qualified_modules": False,
-            "inline": True,
-            "allow_unknown_functions": True,
-            "user_functions": user_functions,
-        }
-    )
-    # Use Pennylane numpy for sympy lambdify
-    modules = pnp
-
-    # The functions down below can be used to switch between different gradient engines
-    # as tensorflow, jax and torch. However, this is not supported and implemented yet.
-
-    #     # SymPy printer for pennylane numpy implementation has to be set manually,
-    #     # otherwise math functions are used in lambdify instead of pennylane.numpy functions
-    #     from sympy.printing.tensorflow import TensorflowPrinter as Printer  # type: ignore
-
-    #     user_functions = {}
-    #     printer = Printer(
-    #         {
-    #             "fully_qualified_modules": False,
-    #             "inline": True,
-    #             "allow_unknown_functions": True,
-    #             "user_functions": user_functions,
-    #         }
-    #     )  #
-    #     modules = tf
-
-    # elif self._gradient_engine == "jax":
-    #     from sympy.printing.numpy import JaxPrinter as Printer  # type: ignore
-
-    #     user_functions = {}
-    #     printer = Printer(
-    #         {
-    #             "fully_qualified_modules": False,
-    #             "inline": True,
-    #             "allow_unknown_functions": True,
-    #             "user_functions": user_functions,
-    #         }
-    #     )  #
-    #     modules = jnp
-    # elif self._gradient_engine == "torch" or self._gradient_engine == "pytorch":
-    #     from sympy.printing.pycode import PythonCodePrinter as Printer  # type: ignore
-
-    #     user_functions = {}
-    #     printer = Printer(
-    #         {
-    #             "fully_qualified_modules": False,
-    #             "inline": True,
-    #             "allow_unknown_functions": True,
-    #             "user_functions": user_functions,
-    #         }
-    #     )  #
-    #     modules = torch
-
-    # else:
-    #     # tbd for jax and tensorflow
-    #     printer = None
-    #     modules = None
-
-    return printer, modules
 
 
 def _resolve_coefficient(coeff, symbol_tuple, printer, modules):

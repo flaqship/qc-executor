8e6db4549fa5f4b1eec894bef857c797274833bf
Dennis Kleinhans <dennis.kleinhans@ipa.fraunhofer.de>
2026-06-26 09:18:05 +0200

    black

--- src/executor/parameters.py
+++ src/executor/parameters.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from qiskit.circuit.parametervector import ParameterVectorElement as Parameter
 from qiskit.circuit.parametervector import ParameterVector as Parameters
+from qiskit.circuit.parametervector import ParameterVectorElement as Parameter
 
 __all__ = ["Parameter", "Parameters"]
